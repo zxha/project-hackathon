54bc35682c0a2b2f00075821e098bf1d68d5816b
zxha <50351446+zxha@users.noreply.github.com>
2021-07-24 16:29:31 +0700

    commit 2

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Cards, Chart, Provinsi } from './index';
 import styles from './app.modules.css';
+import { fetchData } from './api';
 
 class home extends React.Component {
+    state = {
+        data: {},
+        country: '',
+    }
+
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData });
+    }
+
+    handleCountryChange = async (country) => {
+        console.log(country);
+    }
     render(){
+        const{ data } = this.state;
         return (
             <div className="container">
-                <Cards/>
-                <Provinsi/>
+                <Cards data={data}/>
+                <Provinsi handleCountryChange={this.handleCountryChange}/>
                 <Chart/>
             </div>
         )
